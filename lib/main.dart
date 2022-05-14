16f86380abe623ec58d70c5af5f10e7d28fd509a
b4ljk <baljinnyam.dayan@gmail.com>
2022-05-14 23:22:21 +0800

    sex

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        primarySwatch: Colors.purple,
       ),
       initialRoute: '/',
     );
